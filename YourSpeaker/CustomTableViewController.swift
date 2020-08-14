7e7f00f4d4da37a7b1f4e1cbe03a30a67dfa0320
Sumit K Agarwal <sumit@sumits-mbp.fios-router.home>
2020-08-13 23:39:05 -0400

    Add UserDefaults persistence

--- YourSpeaker/CustomTableViewController.swift
+++ YourSpeaker/CustomTableViewController.swift
@@ -7,13 +7,21 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CustomTableViewController: UITableViewController {
 
+    var messageArray = [String]()
     
+    let defaults = UserDefaults.standard
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let items = defaults.array(forKey: "messageArray") as? [String] {
+            messageArray = items
+        }
 
     }
 
@@ -21,25 +29,96 @@ class CustomTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return messageArray.count
     }
 
 
+    // MARK: - Table view delegate
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath)
 
         // Configure the cell...
-
+        cell.textLabel!.text = messageArray[indexPath.row]
+        
         return cell
     }
- 
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let message = messageArray[indexPath.row]
+        let utterance = AVSpeechUtterance(string: message)
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-GB")
+        utterance.rate = 0.5
+        
+        let synthesizer = AVSpeechSynthesizer()
+        synthesizer.speak(utterance)
+        
+        tableView.deselectRow(at: indexPath, animated: true)
 
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            
+            self.messageArray.remove(at: indexPath.row)
+            self.defaults.set(self.messageArray, forKey: "messageArray")
+
+            self.tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    //MARK: - IB Actions
+    
+    @IBAction func addCustom(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        //uialert controller
+        let alert = UIAlertController(title: "Add New Custom Message", message: "", preferredStyle: .alert)
+        
+        //add textfield
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Enter new message"
+            textField = alertTextField
+        }
+        
+        //add action
+        let action = UIAlertAction(title: "Add Customization", style: .default) { (action) in
+            //what will happen when user clicks Add Custom button on our alert
+            //append textfield into messageArray
+            
+            
+            let newMessage = textField.text!
+            self.messageArray.append(newMessage)
+            
+            self.defaults.set(self.messageArray, forKey: "messageArray")
+
+
+            self.tableView.reloadData()
+
+            
+        }
+        
+        
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+        
+    }
+    
+    
+    @IBAction func doneClicked(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    
    
  
 
